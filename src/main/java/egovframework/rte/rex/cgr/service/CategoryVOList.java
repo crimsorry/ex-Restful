9e652e2cfa6479004fd2441d727cfdb6a14a5a67
noksm2 <noksm2@naver.com>
2021-06-16 17:33:54 +0900

    new

---
+++ src/main/java/egovframework/rte/rex/cgr/service/CategoryVOList.java
@@ -0,0 +1,19 @@
+package egovframework.rte.rex.cgr.service;
+
+import java.util.List;
+
+public class CategoryVOList {
+
+	private List<CategoryVO> items;
+
+	public List<CategoryVO> getItems() {
+		return items;
+	}
+
+	public void setItems(List<CategoryVO> items) {
+		this.items = items;
+	}
+	
+	
+	
+}
